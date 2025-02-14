194c90a50cc80b1374516a40f070ebd1f59d7fa9
Akki Aryan <akkiaryan777@gmail.com>
2025-02-15 02:06:40 +0530

    landing pages is done for today will work on the layout and dashboard

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+type Props = { children: React.ReactNode }
+
+const Layout = (props: Props) => {
+  return (
+    <div className="flex overflow-hidden h-screen">
+     
+    </div>
+  )
+}
+
+export default Layout
